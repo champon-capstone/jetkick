831cfea2aaa1c2be3fb2111159d490233d7ef57d
Seokjin Yoon <sjy5386@gmail.com>
2021-04-10 00:08:07 +0900

    Update PlayerCamera

--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     public Transform target;
-    Transform camera;
+    public Transform camera;
+    public Transform pivot;
 
     public float distance = 13.0f;
     public float height = 8.0f;
     public float smoothTime = 0.3f;
     Vector3 velocity = Vector3.zero;
+
+    public bool automatic = true;
+
+    public float followSpeed = 3;
+    public float mouseSpeed = 2;
+
+    float turnSmoothing = .1f;
+    public float minAngle = -35;
+    public float maxAngle = 35;
+
+    float smoothX;
+    float smoothY;
+    float smoothXvelocity;
+    float smoothYvelocity;
+    public float lookAngle;
+    public float tiltAngle;
 
     struct Preset
     {
@@ -34,9 +51,15 @@ public class PlayerCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        camera = transform;
-        camera.position = target.position + new Vector3(0, height, 0) + target.forward * -distance;
-        camera.LookAt(target);
+        if (automatic)
+        {
+            transform.position = target.position + new Vector3(0, height, 0) + target.forward * -distance;
+            camera.LookAt(target);
+        }
+        else
+        {
+            pivot.position = target.position;
+        }
     }
 
     // Update is called once per frame
@@ -50,16 +73,89 @@ public class PlayerCamera : MonoBehaviour
             distance = presets[presetIndex].distance;
             height = presets[presetIndex].height;
         }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            automatic = !automatic;
+            if (automatic)
+            {
+                camera.localPosition = new Vector3(0, 0, 0);
+                transform.position = target.position + new Vector3(0, height, 0) + target.forward * -distance;
+                camera.LookAt(target);
+            }
+            else
+            {
+                camera.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+        }
     }
 
     void FixedUpdate()
     {
+        if (!automatic)
+        {
+            float h = Input.GetAxis("Mouse X");
+            float v = Input.GetAxis("Mouse Y");
 
+            transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * followSpeed);
+            HandleRotations(Time.deltaTime, v, h, mouseSpeed);
+        }
     }
 
     void LateUpdate()
     {
-        camera.position = Vector3.SmoothDamp(transform.position, target.position + new Vector3(0, height, 0) + target.forward * -distance, ref velocity, smoothTime);
-        camera.LookAt(target);
+        if (automatic)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.position + new Vector3(0, height, 0) + target.forward * -distance, ref velocity, smoothTime);
+            camera.LookAt(target);
+        }
+        else
+        {
+            pivot.position = target.position;
+
+            float dist = distance + 1.0f;
+            Ray ray = new Ray(camera.parent.position, camera.position - camera.parent.position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, dist))
+            {
+                if (hit.transform.tag == "Wall")
+                {
+                    dist = hit.distance - 0.25f;
+                }
+            }
+
+            if (dist > distance)
+            {
+                dist = distance;
+            }
+            camera.localPosition = new Vector3(0, 0, -dist);
+        }
+    }
+
+    void HandleRotations(float d, float v, float h, float targetSpeed)
+    {
+        if (automatic)
+        {
+            return;
+        }
+
+        if (turnSmoothing > 0)
+        {
+            smoothX = Mathf.SmoothDamp(smoothX, h, ref smoothXvelocity, turnSmoothing);
+            smoothY = Mathf.SmoothDamp(smoothY, v, ref smoothYvelocity, turnSmoothing);
+        }
+        else
+        {
+            smoothX = h;
+            smoothY = v;
+        }
+
+        tiltAngle -= smoothY * targetSpeed;
+        tiltAngle = Mathf.Clamp(tiltAngle, minAngle, maxAngle);
+        pivot.localRotation = Quaternion.Euler(tiltAngle, 0, 0);
+
+        lookAngle += smoothX * targetSpeed;
+        transform.rotation = Quaternion.Euler(0, lookAngle, 0);
     }
 }
