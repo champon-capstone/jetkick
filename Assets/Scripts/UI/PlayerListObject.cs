1a8a7ad31b81a06b0dad4c63f3c0d2d07de91afe
Arakene <arakene_dev@naver.com>
2021-04-23 21:38:48 +0900

    Add Color dropdown

--- Assets/Scripts/UI/PlayerListObject.cs
+++ Assets/Scripts/UI/PlayerListObject.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using ExitGames.Client.Photon;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
@@ -9,14 +10,21 @@ public class PlayerListObject : MonoBehaviour
     [Header("UI References")] 
     public Text PlayerNameText;
     public Image PlayerColor;
+    public Dropdown ColorDropdown;
 
     private int ownerId;
-
+    private Dictionary<string, Color> colorMap;
+    
     public void Start()
     {
         Hashtable initialProps = new Hashtable() {{GameManager.PLAYER_LIVES, GameManager.PLAYER_MAX_LIVES}};
         PhotonNetwork.LocalPlayer.SetCustomProperties(initialProps);
         PhotonNetwork.LocalPlayer.SetScore(0);
+        colorMap = new Dictionary<string, Color>();
+        colorMap.Add("Black", Color.black);
+        colorMap.Add("Blue", Color.blue);
+        colorMap.Add("Red", Color.red);
+        colorMap.Add("Green", Color.green);
     }
 
     public void Initialize(int playerId, string playerName)
@@ -24,5 +32,10 @@ public class PlayerListObject : MonoBehaviour
         ownerId = playerId;
         PlayerNameText.text = playerName;
         PlayerColor.gameObject.SetActive(true);
+    }
+
+    public void ChangeColor()
+    {
+        PlayerColor.color = colorMap[ColorDropdown.options[ColorDropdown.value].text];
     }
 }
