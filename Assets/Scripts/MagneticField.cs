3d7b86b3d597ad54eec6b59a54ba4728d1e948c2
Seokjin Yoon <sjy5386@gmail.com>
2021-04-26 16:35:43 +0900

    Update MagneticField script

--- Assets/Scripts/MagneticField.cs
+++ Assets/Scripts/MagneticField.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MagneticField : MonoBehaviour
 {
+    public float speed = 100.0f;
+
+    float delta = 0.01f;
+    float interval = 0.01f;
+    float time = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        interval = delta / speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (time > interval)
+        {
+            transform.localScale -= new Vector3(delta, delta, delta);
+            time = 0.0f;
+        }
+        time += Time.deltaTime;
     }
 }
