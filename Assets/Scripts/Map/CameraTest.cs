a1e34f24e253c636107d4e2bc328049e3603d6fc
unnokid <kimki0609@gmail.com>
2021-05-06 21:18:34 +0900

    Update Obstacle Map Cannon

---
+++ Assets/Scripts/Map/CameraTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTest : MonoBehaviour
+{
+    public Camera Carcamera;
+    public Camera Mapcamera;
+
+    public static bool die = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Carcamera.enabled = true;
+        Mapcamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Cardead()
+    {
+        Carcamera.enabled = false;
+        Mapcamera.enabled = true;
+    }
+}
