9d4b814575029d6b0619177513cb7495b8f69024
unnokid <kimki0609@gmail.com>
2021-05-26 20:36:42 +0900

    Update Itembox and Particle

---
+++ Assets/Scripts/Item/ItemController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+
+    public GameObject effect;
+    public float respawnTime;
+    private float spinspeed = 20.0f;
+    Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //renderer = this.GetComponent<Renderer>();
+        renderer = this.GetComponentInChildren<Renderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = spinspeed * Time.deltaTime;
+        transform.Rotate(Vector3.up * y);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag =="Player")
+        {
+            Instantiate(effect, this.transform);
+            //Destroy는 파티클프리팹에 직접넣었음
+            StartCoroutine("FadeOut");
+         
+            Invoke("respawn", respawnTime);
+        }
+    }
+
+    void respawn()
+    {
+        StartCoroutine("FadeIn");
+    }
+
+
+    IEnumerator FadeOut()
+    {
+        int i = 10;
+        while (i > 0)
+        {
+            i -= 1;
+            float f = i / 10.0f;
+            Color c = renderer.material.color;
+            c.a = f;
+            renderer.material.color = c;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        int i = 0;
+        while (i < 10)
+        {
+            i += 1;
+            float f = i / 10.0f;
+            Color c = renderer.material.color;
+            c.a = f;
+            renderer.material.color = c;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+}
