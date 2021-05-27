ee62215eb4c4c412dd60ed0e40ff92d197a704f0
unnokid <kimki0609@gmail.com>
2021-05-28 00:04:12 +0900

    Update Item tag

--- Assets/Scripts/Item/ItemController.cs
+++ Assets/Scripts/Item/ItemController.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemController : MonoBehaviour
 {
+    private GameObject[] item;//획득한 아이템 추가 배열
 
     public GameObject effect;
     public float respawnTime;
     private float spinspeed = 20.0f;
     Renderer renderer;
     public Canvas Canvas;
+
+    public Image firstitem;
+    public Image seconditem;
+
+
+    public Image missile;
+    public Image shield;
+    public Image banana;
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +44,7 @@ public class ItemController : MonoBehaviour
         {
             Instantiate(effect, this.transform);
             //Destroy는 파티클프리팹에 직접넣었음
+            plusItem();
             StartCoroutine("FadeOut");
 
             //아이템 획득 코드
@@ -73,5 +85,10 @@ public class ItemController : MonoBehaviour
             renderer.material.color = c;
             yield return new WaitForSeconds(0.02f);
         }
+    }
+
+    private void plusItem()
+    {
+
     }
 }
