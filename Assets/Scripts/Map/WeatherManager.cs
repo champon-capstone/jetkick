303b7550476ad89fa11d0beacae0f15b18fe1ace
Seokjin Yoon <sjy5386@gmail.com>
2021-05-27 11:22:52 +0900

    Add WeatherManager

---
+++ Assets/Scripts/Map/WeatherManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+    public Weather weather;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
