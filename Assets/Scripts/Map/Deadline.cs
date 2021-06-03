ad8954ca64760303b026a81add16b59c7333f53f
unnokid <kimki0609@gmail.com>
2021-06-03 17:35:02 +0900

    Delete CameraTest Scripts and Fixed Deanline Scripts

--- Assets/Scripts/Map/Deadline.cs
+++ Assets/Scripts/Map/Deadline.cs
@@ -7,11 +7,11 @@ public class Deadline : MonoBehaviour
 {
     public Camera camera;
     public PlayerCamera playerCamera;
-    private CameraTest cameraManager;
+    
     // Start is called before the first frame update
     void Start()
     {
-        cameraManager = GetComponent<CameraTest>();
+        
     }
     void OnTriggerEnter(Collider col)
     {
@@ -20,13 +20,7 @@ public class Deadline : MonoBehaviour
             GameObject BigExplosion;
             BigExplosion = Resources.Load("BigExplosion") as GameObject;
             Instantiate(BigExplosion, col.gameObject.transform.position, Quaternion.identity);
-            if (cameraManager != null)
-            {
-                Debug.Log("cameraManger test");
-                cameraManager.Carcamera.transform.parent.parent.gameObject.GetComponent<PlayerCamera>().enabled = false;
-                Destroy(cameraManager.Carcamera.transform.parent.parent.gameObject);
-                cameraManager.Cardead();
-            }
+          
         }
 
         if (PhotonNetwork.LocalPlayer.ActorNumber == col.transform.parent.GetComponent<MultiCar>().GetActorNumber())
