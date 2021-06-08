10683aac901a66f82382da8e7f572fca4ef3ab93
unnokid <kimki0609@gmail.com>
2021-06-08 21:41:41 +0900

    Update Cars Image and CarSelectPanel

--- Assets/Scripts/Garage/GarageManager.cs
+++ Assets/Scripts/Garage/GarageManager.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GarageManager : MonoBehaviour
 {
+    public Text CarName;
+    public Text CarDetail;
+    public GameObject CarSpawnPoint;
+
+    private string[] Names = {"차량1번임","차량2번임","차량3번임" ,"차량1번임","차량5번임" };
+    private string[] Details = { "이차는 1번으로써 아무튼 1번임",
+                                 "이차는 2번으로써 아무튼 2번임",
+                                 "이차는 3번으로써 아무튼 3번임",
+                                 "이차는 4번으로써 아무튼 4번임",
+                                 "이차는 5번으로써 아무튼 5번임" };
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +25,20 @@ public class GarageManager : MonoBehaviour
     {
         
     }
+
+    public void CallCar(int i)
+    {
+        /*Debug.Log("CallCar 불림");
+        Debug.Log("넘어온 값"+ i);
+        Debug.Log(Names[i]);
+        Debug.Log(Details[i]);*/
+        //choose car number is i
+        CarName.text = Names[i];
+        CarDetail.text = Details[i];
+
+        //Instantiate part
+        //Instantiate(BigExplosion, TargetCar[i].transform.position, Quaternion.identity);
+    }
+
+    
 }
