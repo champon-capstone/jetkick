1d2efbccb676a8f03ae92aedf34629d998ddb1ed
Seokjin Yoon <sjy5386@gmail.com>
2021-06-15 21:47:21 +0900

    Add rainIntensity

--- Assets/Scripts/Map/WeatherSettings.cs
+++ Assets/Scripts/Map/WeatherSettings.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WeatherSettings : MonoBehaviour
 {
     public Material skybox;
+    public float rainIntensity = 0;
 
     // Start is called before the first frame update
     void Start()
