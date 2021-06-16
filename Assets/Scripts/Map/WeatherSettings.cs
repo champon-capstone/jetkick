34ea52458b16afcfd8f7a8b1d7b30ebf7e74a334
Seokjin Yoon <sjy5386@gmail.com>
2021-06-16 21:05:36 +0900

    Add lightIntensity

--- Assets/Scripts/Map/WeatherSettings.cs
+++ Assets/Scripts/Map/WeatherSettings.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class WeatherSettings : MonoBehaviour
 {
     public Material skybox;
+
+    [Range(0.0f, 1.0f)]
     public float rainIntensity = 0;
+
+    [Range(0.0f, 1.0f)]
+    public float lightIntensity = 1.0f;
 
     // Start is called before the first frame update
     void Start()
